3dc3d9715aab62cb9ac4c64b2c2f6f6f71840836
IgorsouzaS <45210334+IgorsouzaS@users.noreply.github.com>
2019-11-11 12:10:26 -0300

    Add files via upload

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,8 @@
+import * as http from 'http';
+const app = require('../src/app').default;
+const PORT = 8080;
 
+
+http.createServer(app).listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+});
